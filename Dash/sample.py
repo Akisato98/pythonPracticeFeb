1f8d9c029e8a2b8e31fed40b82ef5222c887f982
AKIKO <nekoduka@gmail.com>
2020-02-24 16:04:21 +0900

    変更

--- Dash/sample.py
+++ Dash/sample.py
@@ -8,7 +8,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 colors = {
-    'background': 'black',
+    'background': 'pink',
     'text': 'white'
 }
 
